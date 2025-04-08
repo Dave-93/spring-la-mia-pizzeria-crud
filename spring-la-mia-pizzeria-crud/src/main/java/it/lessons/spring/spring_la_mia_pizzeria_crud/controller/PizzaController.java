8978261cc54d0a14875ea1c9fd6896b0094203dd
IlTuoNome <difrancesco.davide93@gmail.com>
2025-04-08 17:53:04 +0200

    first commit

---
+++ spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,27 @@
+package it.lessons.spring.spring_la_mia_pizzeria_crud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.lessons.spring.spring_la_mia_pizzeria_crud.model.Pizza;
+import it.lessons.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+
+@Controller
+@RequestMapping("/pizzeria")
+public class PizzaController {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping()
+    public String index(Model model) {
+        List<Pizza> listaPizze = pizzaRepository.findAll();
+        model.addAttribute("list", listaPizze);
+        return "pizzeria/index";
+    }
+}
---
+++ spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,58 @@
+package it.lessons.spring.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizza") 
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(unique=true, nullable=false)
+    private String nome;
+
+    @Column(length=1000)
+    private String descrizione;
+
+    private double prezzo;
+
+    private String foto;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public String getFoto() {
+        return foto;
+    }
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
