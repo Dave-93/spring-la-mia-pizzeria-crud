a66e72f1f98192bb9db20af6e8efaf0ed36c4b5e
IlTuoNome <difrancesco.davide93@gmail.com>
2025-04-13 09:01:53 +0200

    validazione completata

--- spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,17 +1,24 @@
 package it.lessons.spring.spring_la_mia_pizzeria_crud.controller;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomNumberEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.lessons.spring.spring_la_mia_pizzeria_crud.model.Pizza;
 import it.lessons.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzeria")
@@ -20,16 +27,22 @@ public class PizzaController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    @GetMapping()
-    public String index(/*Model model*/) {
-        return "pizzeria/index";
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Double.class,
+            new CustomNumberEditor(Double.class, NumberFormat.getInstance(), true));
     }
 
-    @GetMapping("/elencoPizze")
+    @ModelAttribute("pizza")
+    public Pizza pizzaForm() {
+        return new Pizza();  // Assicurati che venga sempre inizializzato un oggetto Pizza vuoto
+    }
+
+    @GetMapping()
     public String elenco(Model model) {
         List<Pizza> listaPizze = pizzaRepository.findAll();
         model.addAttribute("list", listaPizze);
-        return "pizzeria/elencoPizze";
+        return "pizzeria/index";
     }
 
     @GetMapping("/addPizza")
@@ -38,12 +51,13 @@ public class PizzaController {
         return "pizzeria/addPizza";
     }
     @PostMapping("/addPizza")
-    public String newPizza(@ModelAttribute("pizza") Pizza formPizza, Model model) {
-        /*
-         * 
-         * 
-         */
+    public String newPizza(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model,RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()){
+            model.addAttribute("bindingResult", bindingResult);
+            return "pizzeria/addPizza";
+        }
         pizzaRepository.save(formPizza);
+        redirectAttributes.addFlashAttribute("successMessage", "Pizza creata");
         return "redirect:/pizzeria";
     }    
 }
--- spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ spring-la-mia-pizzeria-crud/src/main/java/it/lessons/spring/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -6,8 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 @Entity
 @Table(name = "pizza") 
 public class Pizza {
@@ -16,16 +17,16 @@ public class Pizza {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     
-    @NotBlank(message = "Nome obbligatorio")
+    @NotBlank(message="Nome obbligatorio")
     @Column(unique=true, nullable=false)
     private String nome;
 
     @Column(length=1000)
     private String descrizione;
 
-    @NotBlank(message = "Prezzo obbligatorio")
-    @Size(min = 1, message="Non può avere un costo di 0!!")
-    private double prezzo;
+    @NotNull(message = "Prezzo obbligatorio")
+    @DecimalMin(value = "1.00", inclusive = true, message="Il prezzo deve essere maggiore di 0")
+    private Double prezzo;
 
     private String foto;
 
@@ -47,10 +48,10 @@ public class Pizza {
     public void setDescrizione(String descrizione) {
         this.descrizione = descrizione;
     }
-    public double getPrezzo() {
+    public Double getPrezzo() {
         return prezzo;
     }
-    public void setPrezzo(double prezzo) {
+    public void setPrezzo(Double prezzo) {
         this.prezzo = prezzo;
     }
     public String getFoto() {
